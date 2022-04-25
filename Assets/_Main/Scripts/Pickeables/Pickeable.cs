af103e01c048e893182f8c5b2a2e36b100844d1c
Huergacho <75048402+Huergacho@users.noreply.github.com>
2022-04-25 14:51:10 -0300

    Final si final

--- Assets/_Main/Scripts/Pickeables/Pickeable.cs
+++ Assets/_Main/Scripts/Pickeables/Pickeable.cs
@@ -8,16 +8,22 @@ public class Pickeable : MonoBehaviour
 {
     [SerializeField] private LayerMask playerLayer;
     private ParticleSystem particles;
+    private bool canAddPoints;
     private void Start()
     {
         particles = GetComponentInChildren<ParticleSystem>();
+        canAddPoints = true;
     }
     private void OnTriggerEnter(Collider other)
     {
         if((playerLayer & (1 << other.gameObject.layer)) > 0)
         {
-            GameManager.Instance.AddItem();
-            particles.Play();
+            if (canAddPoints)
+            {
+                GameManager.Instance.AddItem();
+                particles.Play();
+                canAddPoints = false;
+            }
         }
     }
 }
